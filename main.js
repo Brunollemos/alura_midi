60dd7260789dc9aa37154d8460fed5a502257870
Bruno Lemos <brunollemoscontato@gmail.com>
2022-09-13 16:41:41 -0300

    Criando função anônima com parâmetro para teste

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ let contador = 0;
 
 while (contador < listaDeTeclas.length) {
     
-    listaDeTeclas[contador].onclick = tocaSomPom;    
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom');
+    }    
     contador++
     console.log(contador);
 }
